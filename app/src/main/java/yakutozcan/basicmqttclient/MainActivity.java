c34418b7a1ac60252c8d970d04ad307121d5e4ed
yakut özcan <yakutozcan@gmail.com>
2017-12-26 21:54:29 +0300

    finish

--- app/src/main/java/yakutozcan/basicmqttclient/MainActivity.java
+++ app/src/main/java/yakutozcan/basicmqttclient/MainActivity.java
@@ -1,13 +1,115 @@
+//yakutozcan.blogspot.com
 package yakutozcan.basicmqttclient;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.util.Log;
+import android.view.View;
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+import java.io.UnsupportedEncodingException;
 public class MainActivity extends AppCompatActivity {
+    final  String TAG = "Naber";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button button = findViewById(R.id.btn_send);
+        final EditText ETRead = findViewById(R.id.et_read);
+        final EditText ETTopic = findViewById(R.id.et_topics);
+        final EditText ETMessage = findViewById(R.id.et_msg);
+        String clientId = MqttClient.generateClientId();
+        final MqttAndroidClient client =
+                new MqttAndroidClient(this.getApplicationContext(), "tcp://broker.shiftr.io:1883",
+                        clientId);
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setMqttVersion(MqttConnectOptions.MQTT_VERSION_3_1);
+        options.setUserName("try");
+        options.setPassword("try".toCharArray());
+        try {
+            IMqttToken token = client.connect(options);
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    Log.d(TAG, "onSuccess");
+                    final String topic = ETTopic.getText().toString();
+                    int qos = 1;
+                    try {
+                        final IMqttToken subToken = client.subscribe(topic, qos);
+                        subToken.setActionCallback(new IMqttActionListener() {
+                            @Override
+                            public void onSuccess(IMqttToken asyncActionToken) {
+                                client.setCallback(new MqttCallback() {
+                                    @Override
+                                    public void connectionLost(Throwable throwable) {
+
+                                    }
+                                    @Override
+                                    public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                                        Log.w(TAG, mqttMessage.toString());
+                                        ETRead.append(mqttMessage.toString());
+                                        ETRead.append("\n");
+                                    }
+
+                                    @Override
+                                    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+                                    }
+                                });
+                            }
+
+                            @Override
+                            public void onFailure(IMqttToken asyncActionToken,
+                                                  Throwable exception) {
+                                // The subscription could not be performed, maybe the user was not
+                                // authorized to subscribe on the specified topic e.g. using wildcards
+
+                            }
+                        });
+
+                    } catch (MqttException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Log.d(TAG, "onFailure");
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d(TAG,"Hello");
+                String topic = ETTopic.getText().toString();
+                String payload = ETMessage.getText().toString();
+                byte[] encodedPayload = new byte[0];
+                try {
+                    encodedPayload = payload.getBytes("UTF-8");
+                    MqttMessage message = new MqttMessage(encodedPayload);
+                    client.publish(topic, message);
+                } catch (UnsupportedEncodingException | MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
+
 }
